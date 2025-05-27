070dfb7bae2ada490dd15ea522074ff45383a503
MoaazAtik <moaaz.atik@gmail.com>
2025-05-27 18:13:10 +0300

    refactor: Move MyApp widget to app.dart and rename to SkillSwapApp
    This commit refactors the main application widget by:
    
    - Moving the `MyApp` widget from `lib/main.dart` to a new file `lib/app.dart`.
    - Renaming the `MyApp` widget to `SkillSwapApp`.
    - Updating `lib/main.dart` to import and use the `SkillSwapApp` widget.
    - Updating `test/widget_test.dart` to use `SkillSwapApp` instead of `MyApp`.
    
    This change improves code organization by separating the main application widget into its own file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:skill_swap/features/auth/controller/auth_gate.dart';
 
+import 'app.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -15,32 +14,7 @@ void main() async {
 
   runApp(
     const ProviderScope(
-      child: MyApp(),
+      child: SkillSwapApp(),
     )
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-
-      localizationsDelegates: [
-        FirebaseUILocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en', 'US'),
-        Locale('tr', 'TR'),
-      ],
-
-      home: const AuthGate(),
-    );
-  }
 }
