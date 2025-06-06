1c1cd76db858750f1ac4a195353c3b5a66ae509e
MoaazAtik <moaaz.atik@gmail.com>
2025-06-06 20:50:12 +0300

    refactor: Move profile providers from controller to UI layer
    This commit moves the `profile_providers.dart` file from the `controller` directory to the `ui` directory within the `profile` feature.
    
    The import path in `edit_profile_screen.dart` has been updated accordingly. This change aligns the location of the providers with their primary usage in the UI layer.

--- lib/features/profile/ui/edit_profile_screen.dart
+++ lib/features/profile/ui/edit_profile_screen.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../../profile/controller/profile_providers.dart';
+import 'profile_providers.dart';
 import '../../profile/domain/models/app_user.dart';
 
 
